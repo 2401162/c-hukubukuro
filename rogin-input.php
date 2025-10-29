8ed95c2ef31759e403874b785c87c847fc5e324c
TOMA4538 <2401204@s.asojuku.ac.jp>
2025-10-29 11:47:55 +0900

    Create rogin-input.html

---
+++ rogin-input.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログインページ</title>
+</head>
+<body>
+    <div class="header">     
+    <h1>福袋販売サイト</h1>
+    <img src="images/logo.png" alt="サイトロゴ">
+    </div>
+    <div class="rogin">
+        <p>メールアドレス</p>
+        <input type="email" name="email" size="40" maxlength="255">
+        <p>パスワード</p>
+        <input type="password" name="password" size="40" maxlength="255">
+        <p>
+        <input type="submit" value="ログイン">
+        </p> 
+        <p><a href="rogin-mail-input.php">パスワードを忘れた方はこちら</a></p>
+        <p><a href="new-member-input.php">新規会員登録はこちら</a></p>
+    </div>
+</body>
+</html>
