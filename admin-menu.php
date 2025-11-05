f998bf01f4fbad8719ddb2536a3d48019d197a0c
obita-mizuki <2401188@s.asojuku.ac.jp>
2025-11-05 11:38:14 +0900

    管理者側ベース

--- admin-menu.php
+++ admin-menu.php
@@ -1,5 +1,8 @@
-<a href="">商品管理</a>
-<a href="">おすすめ更新</a>
-<a href="">顧客管理</a>
-<a href="">レビュー管理</a>
-<hr>
+<nav>
+    <ul>
+        <li><a href="">商品管理</a></li>
+        <li><a href="">おすすめ更新</a></li>
+        <li><a href="">顧客管理</a></li>
+        <li><a href="">レビュー管理</a></li>
+    </ul>
+</nav>
