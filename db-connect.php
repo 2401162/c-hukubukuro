3068d449ff87fcfa067170a8f2ac91b9bf1fd5f5
TOMA4538 <2401204@s.asojuku.ac.jp>
2025-11-05 12:04:35 +0900

    Create db-connect.php

---
+++ db-connect.php
@@ -0,0 +1,12 @@
+<?php
+const SERVER = 'mysql326.phy.lolipop.lan';
+const DBNAME = 'LAA1607624-group';
+const USER = 'LAA1607624';
+const PASS = 'pass0726';
+
+$connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
+// テーブル名（必要ならここで変更）
+const TABLE_MEMBERS = 'members';
+const TABLE_ORDERS = 'orders';
+const TABLE_CARTS = 'carts';
+?>
