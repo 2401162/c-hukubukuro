7cdee14bdcc96acd3b59a136b59fc07f9784ab47
2401162 <2401162@s.asojuku.ac.jp>
2025-11-10 12:42:44 +0900

    henkou

--- admin-customer.php
+++ admin-customer.php
@@ -7,24 +7,23 @@
     <thead>
         <tr>
             <th>名前</th>
-            <th>ユーザーネーム</th>
             <th>メールアドレス</th>
             <th>電話番号</th>
             <th>パスワード</th>
+            <th>住所</th>0
         </tr>
     </thead>
     <tbody>
      <?php
-        $stmt = $pdo->query("SELECT name, username, email, phone, password FROM customers");
+        $stmt = $pdo->query("SELECT name, email, phone, prefecture, city , address_line , password_hash FROM customer");
         foreach ($stmt as $row):
         ?>
         <tr>
           <td><?= htmlspecialchars($row['name']) ?></td>
-          <td><?= htmlspecialchars($row['username']) ?></td>
-          <td><
-          ?= htmlspecialchars($row['email']) ?></td>
+          <td><?= htmlspecialchars($row['email']) ?></td>
           <td><?= htmlspecialchars($row['phone']) ?></td>
-          <td><?= htmlspecialchars($row['password']) ?></td>
+          <td><?= htmlspecialchars($row['password_hash']) ?></td>
+          <td><?= htmlspecialchars($row['prefecture'] . $row['city'] . $row['address_line']) ?></td>
         </tr>
         <?php endforeach; ?>
         </tbody>
---
+++ admin-login-output.php
@@ -0,0 +1,7 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php' ?>
+<?php require 'admin-header.php' ?>
+<?php 
+
+?>
+<?php require 'admin-footer.php' ?>
