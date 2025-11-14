c1f3d04890fb84387e671ce637817f7b7a1d1924
obita-mizuki <2401188@s.asojuku.ac.jp>
2025-11-14 10:55:14 +0900

    Update admin-logout.php

--- admin-logout.php
+++ admin-logout.php
@@ -4,6 +4,6 @@ $_SESSION = []; // セッション変数を空にする
 session_destroy(); // セッションを破棄
 
 // ログインページへリダイレクト（例：admin-login.php）
-header('Location: admin-login.php');
+header('Location: admin-login-input.php');
 exit;
 ?>
