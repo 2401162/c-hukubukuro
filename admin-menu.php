74e5c123d592fec0dfa6b6142d7ca8cdfde96fba
2401162 <2401162@s.asojuku.ac.jp>
2025-11-21 09:47:06 +0900

    Update admin-menu.php

--- admin-menu.php
+++ admin-menu.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
         <li><a href="admin-product.php">商品管理</a></li>
-        <li><a href="">おすすめ更新</a></li>
-        <li><a href="">顧客管理</a></li>
+        <li><a href="admin-recommend.php">おすすめ更新</a></li>
+        <li><a href="admin-customer.php">顧客管理</a></li>
         <li><a href="admin-review.php">レビュー管理</a></li>
     </ul>
 </nav>
